36668de77f9d3d577d66ffb9ca384e842bc0a216
Megatron2032 <1270963204@qq.com>
2018-11-10 21:07:11 +0800

    first commit

---
+++ NeuronLayerBox1.0/Interface/c/init.c
@@ -0,0 +1,96 @@
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/time.h>
+#include <sys/shm.h>
+#include <termios.h>
+//gcc -fPIC -shared Interface/c/init.c Interface/c/input.c Interface/c/output.c -o libNLB.so
+struct shared_use_st
+{
+    int written;//作为一个标志，非0：表示可读，0表示可写
+    int _switch_;
+    int timen;
+    int Nlines;
+    int input_num;
+    int output_num;
+    int step;
+    int output_spike;   //1:spike  0:fmri
+    int model;  //1:step  0:realtime
+    int save;
+    int restore;
+    char restore_file[100];
+};
+
+#define OUTPUT_LAYER 9
+
+struct shared_use_st* NLB_init(int step_ms,int model)
+{
+  /*****************共享内存设置***************************/
+   void *shm = NULL;
+   struct shared_use_st *shared = NULL;
+   int shmid;
+   //创建共享内存
+   shmid = shmget((key_t)2886, sizeof(struct shared_use_st), 0666|IPC_CREAT);
+   if(shmid == -1)
+   {
+       fprintf(stderr, "shmget failed\n");
+       exit(EXIT_FAILURE);
+   }
+   else
+   {printf( "shared_use_st=%d\n",shmid);}
+   //将共享内存连接到当前进程的地址空间
+   shm = shmat(shmid, (void*)0, 0);
+   if(shm == (void*)-1)
+   {
+       fprintf(stderr, "shmat failed\n");
+       exit(EXIT_FAILURE);
+   }
+   printf("Memory attached at %ld\n", (long)shm);
+   //设置共享内存
+   shared = (struct shared_use_st*)shm;
+
+ /*********************************************/
+   printf("wait signal\n");
+   while(shared->written!=1);
+   printf("signal come\n");
+   int c;
+   int i=0,j=0;
+   int n;
+   int Nlines=shared->Nlines;
+   FILE *fout = fopen("load_data/NeuronLayerAddr.txt", "r");
+   if(!fout)
+   {
+    printf("can't open load_data/NeuronLayerAddr.txt\n");
+   }
+   printf("Nlines=%d\n",Nlines);
+   int layeraddr[Nlines];
+   i=0;
+   while (!feof(fout))
+   {
+       fscanf(fout,"%d",&n);
+       layeraddr[i]=n;
+       if(i<Nlines)
+       {
+         printf("layeraddr[%d]=%d\n",i,layeraddr[i]);
+       }
+       i++;
+   }
+   fclose(fout);
+
+   shared->written=0;
+   shared->step=step_ms;
+   shared->model=model;
+
+  return shared;
+}
+
+void NLB_step(int step_ms,struct shared_use_st *shared)
+{
+  if(shared->model)
+  {
+  while(shared->step>0);
+  shared->step=step_ms;
+  while(shared->step>0);
+  }
+}
---
+++ README.md
@@ -0,0 +1,37 @@
+# NeuronLayerBox
+Computational Neuroscience platform with cuda.<br>
+This platform is mainly for calculating stephen grossberg's model.<br>
+1.How to run:<br>
+In NeuronLayerBox file to recompile:<br>
+make clean<br>
+make<br>
+./NLB<br>
+open anthor terminal:python main.py,you will see this:<br>
+this picture show the fmri of layer5 in SMART model<br>
+![input image](NeuronLayerBox1.0/load_data/input.bmp)<br>
+![output layer5](NeuronLayerBox1.0/pic/test1.gif)<br>
+Note:"./test" must be first.if there are some issues,you will clean the shared memory or restart your os. <br>
+2.how to use it:<br>
+you can refer main.py<br>
+your python file must live in NeuronLayerBOx1.0 file.<br>
+2.1:init<br>
+import numpy as np<br>
+import sys<br>
+sys.path.append("Interface/python/")<br>
+from init import NeuronLayerBox<br>
+NLB=NeuronLayerBox(step_ms=1,model=1,spike=0,restore=1)<br>
+model=1:like gym,u can control step time,model=0:real time,u can't control step time,but u can get the output anytime.<br>
+spike=0:output fmri data,spike=1:output spike data that indlude the last ten bits of data.<br>
+restore=1:restore last trained model,restore=0:train a new model without pertrained model.<br>
+2.2:process<br>
+input_src=[]<br>
+input_src.append(some input 2-D array) the num of array must equal input num.In simple terms,len(input_src)=input_num<br>
+NLB.input(input_src) input must be list<br>
+for i in range(50):<br>
+  NLB.step(5)  process 5ms if model=1.if model=0,this will return immediately.<br>
+2.3:save model<br>
+NLB.save()<br>
+it will save model in load_data/SaveData.NLB terminal will print information about model's name.<br>
+2.4:exit<br>
+NLB.exit()<br>
+this will delete shared memory and release NLB.<br>
